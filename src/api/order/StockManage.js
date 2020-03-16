ccc203837fe07e120757525e138e802a801c7d65
liangjuan <1176541527@qq.com>
2020-03-16 10:53:30 +0800

    修改接口

--- src/api/order/StockManage.js
+++ src/api/order/StockManage.js
@@ -4,22 +4,22 @@ import baseHttpProvider from '../base/baseHttpProvider';
 
 
 const updateStock = (params) => {
-  return baseHttpProvider.postFormApi('api/stock/updateStock', params)
+  return baseHttpProvider.postFormApi('api/str/stock/updateStock', params)
 }
 
 const getStockList = (params) => {
-  return baseHttpProvider.postFormApi('api/stock/searchList', { page: 1, size: 10, ...params }, { total: true })
+  return baseHttpProvider.postFormApi('api/str/stock/searchList', { page: 1, size: 10, ...params }, { total: true })
 }
 
 const getDetail = (params) => {
-  return baseHttpProvider.getApi('api/stock/getDetail', params)
+  return baseHttpProvider.getApi('api/str/stock/getDetail', params)
 }
 
 const getStockLogList = (params) => {
-  return baseHttpProvider.postFormApi('api/stockLog/searchList', { page: 1, size: 10, ...params }, { total: true })
+  return baseHttpProvider.postFormApi('api/str/stockLog/searchList', { page: 1, size: 10, ...params }, { total: true })
 }
 const batchDeleteStatus = (params) => {
-  return baseHttpProvider.getApi('api/stockLog/batchDeleteStatus', params)
+  return baseHttpProvider.getApi('api/str/stockLog/batchDeleteStatus', params)
 }
 
 export {
