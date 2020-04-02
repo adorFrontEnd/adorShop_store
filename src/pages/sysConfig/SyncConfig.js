dc14fbf464e71a90bcae5ecc93c5419be0fcec08
masecho <masecho@163.com>
2020-04-02 17:32:00 +0800

    修改同步网店管家配置

--- src/pages/sysConfig/SyncConfig.js
+++ src/pages/sysConfig/SyncConfig.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import CommonPage from '../../components/common-page';
 import { Table, Form, Input, Col, Switch, Row, Button, Modal } from "antd";
-import { getHousekeeperConfig, updateHousekeeperConfig} from '../../api/sysConfig/sysConfig'
+import { getHousekeeperConfig, updateHousekeeperConfig } from '../../api/sysConfig/sysConfig'
 import Toast from '../../utils/toast';
 const _title = "网店管家";
 
@@ -18,43 +18,41 @@ class Page extends Component {
   getPageData = () => {
     getHousekeeperConfig()
       .then(pageData => {
-        if (pageData.status == 0) {
-          this.setState({ swichStatus: false })
-        } else {
-          this.setState({ swichStatus: true })
-        }
+
         this.setState({
-          pageData
+          pageData,
+          swichStatus: pageData && pageData.status == 1
         })
         if (!pageData || !pageData.id) {
           this.props.form.setFieldsValue({ appId: "", appSecret: "", tokenStr: "" });
-          this.setState({
-            status: false
-          })
           return;
         }
         let { appId, appSecret, status, tokenStr } = pageData;
-        this.setState({
-          status: status == '1'
-        })
         this.props.form.setFieldsValue({ appId, appSecret, tokenStr });
       })
   }
 
   onSyncStatusChange = (swichStatus) => {
-    // let title = status ? "开启" : "关闭";
-    // Toast(`${title}同步网店管家成功！`);
-if(swichStatus){
-  this.getPageData()
-}
     this.setState({
       swichStatus
     })
   }
-  saveClick = (swichStatus) => {
+
+  saveClick = () => {
+    let { swichStatus, pageData } = this.state;
     let title = swichStatus ? "开启" : "关闭";
-    let status=swichStatus?1:0
-    let pageData = this.state.pageData;
+    let status = swichStatus ? 1 : 0;
+    let id = pageData && pageData.id ? pageData.id : null;
+    if (!swichStatus) {
+      let { appId, appSecret, tokenStr } = this.props.form.getFieldsValue();
+      updateHousekeeperConfig({ id, status, appId, appSecret, tokenStr })
+        .then((data) => {
+          Toast(`${title}网店管家同步成功！`);
+          this.getPageData()
+        })
+      return;
+    }
+
     this.props.form.validateFields((err, params) => {
       if (err) {
         return;
@@ -63,9 +61,9 @@ if(swichStatus){
       this.setState({
         swichStatus
       })
-      updateHousekeeperConfig({ id:pageData.id, ...params,status })
+      updateHousekeeperConfig({ id, appId, appSecret, tokenStr, status })
         .then((data) => {
-          Toast(`${title}同步网店管家成功！`);
+          Toast(`${title}网店管家同步成功！`);
           this.getPageData()
         })
     })
@@ -77,73 +75,73 @@ if(swichStatus){
         <>
           <>
             <div className='font-20 line-height40 font-bold'>网店管家</div>
-            <div className='line-height40'>
-              <span className='margin-right20'>网店管家同步</span>
-              <Switch checked={this.state.swichStatus} onChange={this.onSyncStatusChange} />
-            </div>
-            {
-              this.state.swichStatus ?
-                <Form style={{ width: 500 }} className='common-form'>
-                  <Form.Item
-                    field="appId"
-                    label="appId"
-                    labelCol={{ span: 6 }}
-                    wrapperCol={{ span: 16 }}
-                  >
-                    {
-                      getFieldDecorator('appId', {
-                        rules: [
-                          { required: true, message: '请设置appId!' }
-                        ],
-                        initialValue: null
-                      })(
-                        <Input autoComplete="false" placeholder='请设置appId' />
-                      )
-                    }
-                  </Form.Item>
-                  <Form.Item
-                    field="appSecret"
-                    label="appSecret"
-                    labelCol={{ span: 6 }}
-                    wrapperCol={{ span: 16 }}
-                  >
-                    {
-                      getFieldDecorator('appSecret', {
-                        rules: [
-                          { required: true, message: '请设置appSecret!' }
-                        ],
-                        initialValue: null
-                      })(
-                        <Input.Password autoComplete="false" visibilityToggle={false} placeholder='请设置AppSecret' />
-                      )
-                    }
-                  </Form.Item>
-                  <Form.Item
-                    field="tokenStr"
-                    label="token"
-                    labelCol={{ span: 6 }}
-                    wrapperCol={{ span: 16 }}
-                  >
-                    {
-                      getFieldDecorator('tokenStr', {
-                        rules: [
-                          { required: true, message: '请设置token!' }
-                        ],
-                        initialValue: null
-                      })(
-                        <Input autoComplete="false" placeholder='请设置Token' />
-                      )
-                    }
-                  </Form.Item>
+            <Row style={{ width: 500 }} className='line-height40 margin-bottom'>
+              <Col span={6} style={{ textAlign: "right" }}>
+                网店管家同步：
+              </Col>
+              <Col span={16}>
+                <Switch checked={this.state.swichStatus} onChange={this.onSyncStatusChange} />
+              </Col>
+            </Row>
 
-                  <Row className='line-height40'>
-                    <Col offset={6}>
-                      <Button onClick={this.saveClick} type='primary' className='normal'>保存</Button>
-                    </Col>
-
-                  </Row>
-                </Form> : null
-            }
+            <Form style={{ width: 500, display: `${this.state.swichStatus ? 'block' : "none"}` }} className='common-form'>
+              <Form.Item
+                field="appId"
+                label="appId"
+                labelCol={{ span: 6 }}
+                wrapperCol={{ span: 16 }}
+              >
+                {
+                  getFieldDecorator('appId', {
+                    rules: [
+                      { required: true, message: '请设置appId!' }
+                    ],
+                    initialValue: null
+                  })(
+                    <Input autoComplete="false" placeholder='请设置appId' />
+                  )
+                }
+              </Form.Item>
+              <Form.Item
+                field="appSecret"
+                label="appSecret"
+                labelCol={{ span: 6 }}
+                wrapperCol={{ span: 16 }}
+              >
+                {
+                  getFieldDecorator('appSecret', {
+                    rules: [
+                      { required: true, message: '请设置appSecret!' }
+                    ],
+                    initialValue: null
+                  })(
+                    <Input.Password autoComplete="false" visibilityToggle={false} placeholder='请设置AppSecret' />
+                  )
+                }
+              </Form.Item>
+              <Form.Item
+                field="tokenStr"
+                label="token"
+                labelCol={{ span: 6 }}
+                wrapperCol={{ span: 16 }}
+              >
+                {
+                  getFieldDecorator('tokenStr', {
+                    rules: [
+                      { required: true, message: '请设置token!' }
+                    ],
+                    initialValue: null
+                  })(
+                    <Input autoComplete="false" placeholder='请设置Token' />
+                  )
+                }
+              </Form.Item>
+            </Form>
+            <Row className='line-height40' style={{ width: 500 }}>
+              <Col offset={6}>
+                <Button onClick={this.saveClick} type='primary' className='normal'>保存</Button>
+              </Col>
+            </Row>
 
           </>
 
